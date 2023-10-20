ea69e71e7e571d6e54636abe37f7ca076fc4231d
Piotr Bobiński WSB <62461206+pbobinski-WSB@users.noreply.github.com>
2023-10-20 09:19:36 +0200

    Pakiet bankowy - początek

---
+++ src/wsb/po/banking/BankAccountTester.java
@@ -0,0 +1,27 @@
+package wsb.po.banking;
+
+public class BankAccountTester {
+    /**
+     * Tests the methods of the BankAccount class.
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        BankAccount harrysChecking = new BankAccount();
+        harrysChecking.deposit(2000);
+        harrysChecking.withdraw(500);
+        System.out.println(harrysChecking.getBalance());
+        System.out.println("Expected: 1500");
+
+        BankAccount inneKonto = new BankAccount(2000);
+        inneKonto.withdraw(500);
+        System.out.println(inneKonto.getBalance());
+
+        System.out.println(harrysChecking == inneKonto);
+        System.out.println(harrysChecking.equals(inneKonto));
+
+        System.out.println(harrysChecking.toString());
+    }
+
+
+}
